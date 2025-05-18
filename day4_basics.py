e055089c6d694bf2fd5c5ab3146275e2887b5aa3
hemantghasti <hemantghasti6@gmail.com>
2025-05-18 13:51:55 +0530

    Add files via upload

---
+++ day4_basics.py
@@ -0,0 +1,34 @@
+# This is a comment
+# It explains what the code does
+print("Hello, world!")
+print("My name is Hemant")
+name = "Hemant"
+age = 16
+is_student = True
+
+print("My name is", name)
+print("My age is", age)
+# String (text)
+language = "Python"
+
+# Integer (whole number)
+year = 2025
+
+# Float (decimal)
+pi = 3.14
+
+# Boolean (True/False)
+smart = True
+user_name = input("Enter your name: ")
+print("Welcome,", user_name)
+age = input("Enter your age: ")
+age = int(age)  # Convert to number
+print("Next year you'll be", age + 1)
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Power:", a ** b)
